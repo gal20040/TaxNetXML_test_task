a832807c2311cb0734e025196384ed480dc26ddf
Artem Grodeckiy <gal20040@gmail.com>
2017-03-26 17:54:10 +0300

    Добавил поодключение к базе и модель User.

---
+++ TaxNetXML/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace TaxNetXML.Models
+{
+    public class User
+    {
+        //[Column(IsPrimaryKey = true, IsDbGenerated = true)]
+        public int    Id   { get; set; } // ID пользователя
+        public string Name { get; set; } // имя пользователя
+        public int    Age  { get; set; } // возраст пользователя
+    }
+}
---
+++ TaxNetXML/Models/UserContext.cs
@@ -0,0 +1,7 @@
+﻿using System.Data.Entity;
+
+namespace TaxNetXML.Models {
+    public class UserContext : DbContext {
+        public DbSet<User> Users { get; set; }
+    }
+}
