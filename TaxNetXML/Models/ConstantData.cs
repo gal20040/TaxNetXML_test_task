18fc9f354e1be0a9998cf55e33c99b944b3e75f3
Artem Grodeckiy <gal20040@gmail.com>
2017-03-31 02:01:59 +0300

    добавил константы.

--- TaxNetXML/Models/ConstantData.cs
+++ TaxNetXML/Models/ConstantData.cs
@@ -1,8 +1,10 @@
 ﻿namespace TaxNetXML.Models {
     public static class ConstantData {
-        public static readonly string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename='|DataDirectory|\FileStore.mdf';Integrated Security=True";
+        public static readonly string CONNECTION_STRING = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename='|DataDirectory|\FileStore.mdf';Integrated Security=True";
         public static readonly string querySelectFromFiles = "SELECT * FROM Files";
-        public static readonly string insertQueryTemplate = "INSERT INTO {0:d} (FileName, FileVersion, ChangeDate) VALUES ('{1:d}', '{2:d}', '{3:d}')";
-        public static readonly string dateFormat = "yyyy-MM-ddTHH:mm:ss.ffzzz";
+        public static readonly string insertQueryTemplate = "INSERT INTO {0:d} (Name, FileVersion, DateTime) VALUES ('{1:d}', '{2:d}', '{3:d}')";
+        public static readonly string dateFormatWithDash = "yyyy-MM-ddTHH:mm:ss.ffzzz";
+        public static readonly string dateFormaForFileName = "yyyy-MM-ddTHH-mm-sszzz";
+        public static readonly string file_type = "application/xml";
     }
 }
