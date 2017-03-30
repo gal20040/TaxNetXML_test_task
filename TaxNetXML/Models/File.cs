d427f87fc2461daf9a4efd677e0b3f8d67e71873
Artem Grodeckiy <gal20040@gmail.com>
2017-03-30 18:06:14 +0300

    перешёл от модели с Users к модели Files.

---
+++ TaxNetXML/Models/File.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TaxNetXML.Models {
+//CREATE TABLE [dbo].[Files] (
+//    [Id] INT IDENTITY(1, 1) NOT NULL PRIMARY KEY,
+//    [FileName] NVARCHAR(100) NOT NULL,
+//    [FileVersion] NVARCHAR(50) NOT NULL,
+//    [ChangeDate] DATETIME2(7) NOT NULL
+//);
+
+    //
+    // Summary:
+    //     Описывает структуру File из БД.
+    //
+    // Fields:
+    //   Id:
+    //     Номер записи
+    //   FileName:
+    //     Наименование файла
+    //   FileVersion:
+    //     Версия файла
+    //   ChangeDate:
+    //     Дата изменения файла
+    public class File {
+        public int Id { get; set; }
+        public string FileName { get; set; }
+        public string FileVersion { get; set; }
+        public DateTime ChangeDate { get; set; }
+    }
+}
---
+++ TaxNetXML/Models/FileContext.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.Entity;
+
+namespace TaxNetXML.Models {
+
+    //
+    // Summary:
+    //     Контекст данных для работы с моделью.
+    public class FileContext : DbContext {
+        public DbSet<File> Files { get; set; }
+    }
+}
