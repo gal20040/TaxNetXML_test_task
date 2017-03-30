5c74c030af395eefae6125d007a842acc0c6a7ca
Artem Grodeckiy <gal20040@gmail.com>
2017-03-30 18:06:45 +0300

    вынес константы в отдельный класс.

---
+++ TaxNetXML/Models/ConstantData.cs
@@ -0,0 +1,8 @@
+﻿namespace TaxNetXML.Models {
+    public static class ConstantData {
+        public static readonly string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename='|DataDirectory|\FileStore.mdf';Integrated Security=True";
+        public static readonly string querySelectFromFiles = "SELECT * FROM Files";
+        public static readonly string insertQueryTemplate = "INSERT INTO {0:d} (FileName, FileVersion, ChangeDate) VALUES ('{1:d}', '{2:d}', '{3:d}')";
+        public static readonly string dateFormat = "yyyy-MM-ddTHH:mm:ss.ffzzz";
+    }
+}
