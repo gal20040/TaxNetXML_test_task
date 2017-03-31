44839e5c89505af3f391f11ab292b5e2b87afdaf
Artem Grodeckiy <gal20040@gmail.com>
2017-03-31 07:01:20 +0300

    добавил методы редактирования, просмотра, создания, удаления записей.

--- TaxNetXML/Controllers/HomeController.cs
+++ TaxNetXML/Controllers/HomeController.cs
@@ -1,39 +1,109 @@
-﻿using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
-using System.Data.SqlClient;
+﻿using System.Data.Entity;
+using System.Net;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 using TaxNetXML.Models;
 
 //todo сделать автосоздание бд. если её нет
 //TODO переделать подпись в _Layout.cshtml
-//TODO разобраться с методом Dispose
-//TODO если Entity Framework можно использовать, тогда попробовать реализовать insert и update через фреймворк - см. метод ReadFromXml
+//TODO реализовать insert и update через фреймворк - см. метод ReadFromXml
 
 namespace TaxNetXML.Controllers {
     public class HomeController : Controller {
         private FileContext db = new FileContext();
 
-        //TODO переделать на относительный путь
-        //private string pathToXML; // = "D:\\gal20040\\CSharp\\TaxNetXML\\TaxNetXML\\App_Data\\filesdb.xml";
-        //private static string pathToXML = Server.MapPath("~/App_Data/filesdb.xml");//"~\\App_Data\\filesdb.xml";
-
-        //private const string CONNECTION_STRING = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename='|DataDirectory|\FileStore.mdf';Integrated Security=True";
-        //private const string sql = "SELECT * FROM Files";
-
         //
         // Summary:
         //     Получает из БД список данных и отправляет его в представление пользователю.
         public async Task<ActionResult> Index() {
-            IEnumerable<File> files = await db.Files.ToListAsync();
-            ViewBag.Files = files;
+            return View(await db.Files.ToListAsync());
+        }
 
-            //todo сделать передачу заголовков таблицы
-            //string[] columnHeaders = new string[] { "A", "B", "C" };
-            //ViewBag.ColumnHeaders = columnHeaders;
+        // GET: BackupDB/Create
+        public ActionResult Create() {
+            return View();
+        }
 
-            return View("Index");
+        // POST: BackupDB/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Name,FileVersion,DateTime")] File file) {
+            if (ModelState.IsValid) {
+                db.Files.Add(file);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(file);
+        }
+
+        // GET: BackupDB/Edit/5
+        public async Task<ActionResult> Edit(int? id) {
+            if (id == null) {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            File file = await db.Files.FindAsync(id);
+            if (file == null) {
+                return HttpNotFound();
+            }
+            return View(file);
+        }
+
+        // POST: BackupDB/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Name,FileVersion,DateTime")] File file) {
+            if (ModelState.IsValid) {
+                db.Entry(file).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(file);
+        }
+
+        // GET: BackupDB/Details/5
+        public async Task<ActionResult> Details(int? id) {
+            if (id == null) {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            File file = await db.Files.FindAsync(id);
+            if (file == null) {
+                return HttpNotFound();
+            }
+            return View(file);
+        }
+
+        // GET: BackupDB/Delete/5
+        public async Task<ActionResult> Delete(int? id) {
+            if (id == null) {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            File file = await db.Files.FindAsync(id);
+            if (file == null) {
+                return HttpNotFound();
+            }
+            return View(file);
+        }
+
+        // POST: BackupDB/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id) {
+            File file = await db.Files.FindAsync(id);
+            db.Files.Remove(file);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing) {
+            if (disposing) {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
